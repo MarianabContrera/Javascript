cafbab6035b3321b95ef2d3d7f4f4cda1414279f
Mariana Contrera <marianabelencontrera@gmail.com>
2021-11-08 18:56:03 -0300

    Entrega clase 3 con agregado de condicion en el ciclo

--- Clase 3/19965Contrera/js/miarchivo.js
+++ Clase 3/19965Contrera/js/miarchivo.js
@@ -1,13 +1,19 @@
 
+var dni = 0;
 
 for (let i = 5; i > 0; i--) {
+    
     let ingresarNombre = prompt("Ingresá tu nombre para participar de una rifa. Los primeros 5 estarán participando por un Smart TV!");
-    let ingresarNumero = prompt("Ingresá tu últimos tres números de DNI");
-   
-    alert(" Gracias! " + ingresarNombre + "Tu número es " + ingresarNumero + "Ya estás pasrticipando!");
+    let ingresarNumero = prompt("Ingresá tu DNI");
+    if (ingresarNumero === dni ) {
+      alert("el dni ingresado ya está registrado");
+    } else {
+      alert("Gracias! " + ingresarNombre + "Tu número es " + ingresarNumero + " Ya estás pasrticipando!");
+    }
+
+    dni = ingresarNumero;
+  
   } 
 
   alert("Lo Sentimos mucho, ya no quedan números disponibles");
-  
-  
   
