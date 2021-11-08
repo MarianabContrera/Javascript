4ce4c0f458e1de21edfe013f0076b87682a22df4
Mariana Contrera <marianabelencontrera@gmail.com>
2021-11-07 21:16:52 -0300

    entrega clase 2 y 3

---
+++ Clase 3/19965Contrera/js/miarchivo.js
@@ -0,0 +1,13 @@
+
+
+for (let i = 5; i > 0; i--) {
+    let ingresarNombre = prompt("Ingresá tu nombre para participar de una rifa. Los primeros 5 estarán participando por un Smart TV!");
+    let ingresarNumero = prompt("Ingresá tu últimos tres números de DNI");
+   
+    alert(" Gracias! " + ingresarNombre + "Tu número es " + ingresarNumero + "Ya estás pasrticipando!");
+  } 
+
+  alert("Lo Sentimos mucho, ya no quedan números disponibles");
+  
+  
+  
