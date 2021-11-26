473e7c824025ee12c40962736e6ea377e6d2f3d1
Mariana Contrera <marianabelencontrera@gmail.com>
2021-11-26 10:17:00 -0300

    Entrega clase 8 DOM

---
+++ Clase 8 DOM/19965Contrera - DOM/js/miarchivo.js
@@ -0,0 +1,28 @@
+
+
+/*Crear elementos manipulando el DOM a partir de la informaciòn de tus objetos.*/
+
+const habitaciones = [{ id: 1,  tipo: "Individual", precio: 1000 },
+                  {  id: 2,  tipo: "Doble", precio: 1500 },
+                  {  id: 3,  tipo: "Familiar"  , precio: 2000}, 
+                ]
+
+for (const habitacion of habitaciones) {
+    let contenedor = document.createElement("div");
+    //Definimos el innerHTML del elemento con una plantilla de texto
+    contenedor.innerHTML = `<h3> ID: ${habitacion.id}</h3>
+                            <p>  Producto: ${habitacion.tipo}</p>
+                            <b> $ ${habitacion.precio}</b>`;
+    document.body.appendChild(contenedor);
+}
+
+
+/* Modificar etiquetas existentes en función del resultado de operaciones. */
+
+const locacion = document.querySelector('h1');
+console.log(locacion);
+
+console.log(locacion.innerHTML);
+ 
+document.querySelector('h1').textContent = 'ALAGOAS';
+
